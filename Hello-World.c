0a02b44ea97744eb08429d60668b443c1dec0ffc
Josh Pokorny <joshpokorny@Joshs-MacBook-Air.local>
2018-01-26 21:19:38 -0600

    Hello World

---
+++ Hello-World.c
@@ -0,0 +1,20 @@
+/**
+* Author: Josh Pokorny
+* Date: 01/26/2018
+*
+* This program prints your name
+*/
+
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+	
+	printf("Josh Pokorny\n");
+
+	return 0;
+
+
+	
+
+
+}
